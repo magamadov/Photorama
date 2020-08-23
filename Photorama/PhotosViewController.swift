0bd1929e1d7cdab6648d636a0f9f401f204fd617
Zelimkhan Magamadov <magamadov@gmail.com>
2020-08-23 05:45:16 +0300

    Starting the Photorama Application

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by ZELIMKHAN MAGAMADOV on 23.08.2020.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+  @IBOutlet private var imageView: UIImageView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
