0d88298403083aa5f3bf90cb0b4c0923051f8eae
Zelimkhan Magamadov <magamadov@gmail.com>
2020-08-23 09:43:52 +0300

    Starting the Photorama Application

---
+++ Photorama/FlickrAPI.swift
@@ -0,0 +1,18 @@
+//
+//  FlickrAPI.swift
+//  Photorama
+//
+//  Created by ZELIMKHAN MAGAMADOV on 23.08.2020.
+//
+
+import Foundation
+
+enum EndPoints: String {
+  case interestingPhotos = "flickr.interestingness.getList"
+}
+
+struct FlickrAPI {
+  private static let baseURLString = "https://api.flickr.com/services/rest"
+  
+}
+
--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -13,9 +13,6 @@ class PhotosViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+  
   }
-
-
 }
-
