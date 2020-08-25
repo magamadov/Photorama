5d2b53cea92d1db1424388929363b2596445497b
Zelimkhan Magamadov <magamadov@gmail.com>
2020-08-25 16:53:25 +0300

    Downloading and Displaying the Image Data

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -5,7 +5,12 @@
 //  Created by ZELIMKHAN MAGAMADOV on 24.08.2020.
 //
 
-import Foundation
+import UIKit
+
+enum PhotoError: Error {
+  case imageCreationError
+  case missingImageURL
+}
 
 class PhotoStore {
   
@@ -14,7 +19,7 @@ class PhotoStore {
     return URLSession(configuration: config)
   }()
   
-  func fetchInterestingPhoto(completion: @escaping ((Result<[Photo], Error>) -> Void)) {
+  func fetchInterestingPhoto(completion: @escaping (Result<[Photo], Error>) -> Void) {
     let url = FlickrAPI.interestingPhotosURL
     let request = URLRequest(url: url)
     let task = session.dataTask(with: request) { (data, response, error) in
@@ -22,6 +27,31 @@ class PhotoStore {
       completion(result)
     }
     task.resume()
+  }
+  
+  func fetchImage(for photo: Photo, completion: @escaping (Result<UIImage, Error>) -> Void) {
+    guard let photoURL = photo.remoteURL else {
+      completion(.failure(PhotoError.missingImageURL))
+      return
+    }
+    let request = URLRequest(url: photoURL)
+    let task = session.dataTask(with: request) { (data, response, error) in
+      let result = self.proccessImageRequest(data: data, error: error)
+      completion(result)
+    }
+    task.resume()
+  }
+  
+  private func proccessImageRequest(data: Data?, error: Error?) -> Result<UIImage, Error> {
+    guard let imageData = data,
+          let image = UIImage(data: imageData) else {
+      if data == nil {
+        return .failure(error!)
+      } else {
+        return .failure(PhotoError.imageCreationError)
+      }
+    }
+    return .success(image)
   }
   
   private func processPhotoRequest(data: Data?, error: Error?) -> Result<[Photo], Error> {
--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -17,11 +17,29 @@ class PhotosViewController: UIViewController {
     
     store.fetchInterestingPhoto { (photosResult) in
       switch photosResult {
-        case .success(let photo):
-          print("Successfully found: \(photo.count) photos")
+        case .success(let photos):
+          print("Successfully found: \(photos.count) photos")
+          //if let firstPhoto = photos[Int.random(in: 0..<photos.count)] {
+            self.updateImageView(for: photos[Int.random(in: 0..<photos.count)])
+          //}
         case .failure(let error):
           print("Error fetching interesting photos: \(error)")
       }
     }
   }
+  
+  func updateImageView(for photo: Photo) {
+    store.fetchImage(for: photo) { (imageResult) in
+      switch imageResult {
+        case .success(let image):
+          DispatchQueue.main.async {
+            self.imageView.image = image
+          }
+          
+        case .failure(let error):
+          print("Error downloading image: \(error)")
+      }
+    }
+  }
+  
 }
